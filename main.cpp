47d42137c5568a8b2a975fa63f23b5b4ce411af9
hassan_codex <hsnhackerking@gmail.com>
2026-03-28 19:11:46 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+class salary
+{
+public:
+    int n;
+    int salary1[100];
+    void setValue()
+    {
+        cout<<"enter the n"<<endl;
+        cin>>n;
+        for(int i=0; i<n; i++)
+        {
+            cout<<"enter salary number"<<i<<endl;
+            cin>>salary1[i];
+        }
+    }
+    void getValue()
+    {
+        int total=0;
+        for(int i=0; i<n; i++)
+        {
+            total+=salary1[i];
+        }
+        cout<<"total salary "<<total;
+    }
+};
+
+
+int main()
+{
+   salary s1;
+   s1.setValue();
+   s1.getValue();
+        return 0;
+}
